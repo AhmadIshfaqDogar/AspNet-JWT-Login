c231effd63c1e40da9f0fe35f33731e6abb042d0
Ahmad Ishfaq <ahmadishfaqdogar10@gmail.com>
2025-12-07 11:18:37 +0500

    security added

--- obj/Debug/net10.0/JwtAuthDemo.AssemblyInfo.cs
+++ obj/Debug/net10.0/JwtAuthDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JwtAuthDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+182ed38bb92ea90f5e0c7d11180867d87cfaf5c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("JwtAuthDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JwtAuthDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
